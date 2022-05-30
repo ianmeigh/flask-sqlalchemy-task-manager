93c5a2d56bffaafd66164229ea37f1d257af221b
ianmeigh <ianmeigh@googlemail.com>
2022-05-30 16:25:08 +0100

    feat: create basic flask app

---
+++ config.py
@@ -0,0 +1,31 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+# https://hackersandslackers.com/configure-flask-applications/
+
+# https://www.geeksforgeeks.org/__file__-a-special-variable-in-python/
+# https://dev.to/jakewitcher/using-env-files-for-environment-variables-in-python-applications-55a1
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    """Set Flask configuration from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get("SECRET_KEY")
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("DB_URL")
+
+
+class ProdConfig(Config):
+    FLASK_ENV = "production"
+    TESTING = False
+    DEBUG = False
+
+
+class DevConfig(Config):
+    FLASK_ENV = "development"
+    TESTING = True
+    DEBUG = True
---
+++ setup.py
@@ -0,0 +1,4 @@
+from taskmanager import app
+
+if __name__ == "__main__":
+    app.run()
