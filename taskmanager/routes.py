466461c43cc03105b55459c4a62d24cdb8c3d487
ianmeigh <ianmeigh@googlemail.com>
2022-06-01 18:15:33 +0100

    feat: update categories in the database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -26,3 +26,22 @@ def add_category():
             db.session.rollback()
             return error
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        updated_category_name = request.form.get("category_name")
+        if category.category_name != updated_category_name:
+            category.category_name = updated_category_name
+            try:
+                db.session.commit()
+                return redirect(url_for("categories"))
+            except Exception as error:
+                db.session.rollback()
+                return error
+        else:
+            flash("Name not changed")
+
+    return render_template("edit_category.html", category=category)
