bd80c0d15fc2b5310fba28bd687ab203995452b0
ianmeigh <ianmeigh@googlemail.com>
2022-06-02 17:59:33 +0100

    fix: correct date formatting

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
   M.Modal.init(document.querySelectorAll('.modal'), {});
+
   M.Datepicker.init(document.querySelectorAll('.datepicker'), {
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year
-    format: "dd mm, yyyy",
-    i18n: {done: "Select"},
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" },
+    autoClose: true
   });
   M.FormSelect.init(document.querySelectorAll('select'), {});
   M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
